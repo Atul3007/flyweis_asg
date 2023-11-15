f32c9f6149c85ab89c011117519e5ca22a29eb3c
atul3007 <atuldwivedi859@gmail.com>
2023-11-15 12:17:19 +0530

    new

--- frontend/src/pages/Users.js
+++ frontend/src/pages/Users.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
-import React,{ useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Users = () => {
-  const [users,setUsers]=useState([]);
+  const [users, setUsers] = useState([]);
   const auth = JSON.parse(localStorage.getItem("auth"));
 
   const config = {
@@ -11,48 +11,67 @@ const Users = () => {
       "Content-Type": "application/json", // Specify the content type
     },
   };
-  
+
   const getUsers = async () => {
     try {
-      const {data} = await axios.get("https://shlok-mittal-lawyer-backend.vercel.app/api/v1/admin/users", config);
+      const { data } = await axios.get(
+        "https://shlok-mittal-lawyer-backend.vercel.app/api/v1/admin/users",
+        config
+      );
       setUsers(data.data);
+      console.log(users);
     } catch (error) {
       console.error("Error fetching user data:", error);
     }
   };
-  
+
   useEffect(() => {
     getUsers();
-  }, [auth]);
-  
+  }, []);
+
   return (
     <div>
-       <h1>All Users</h1>
-          {users.map((user, index) => (
-            <div className="border-shadow" key={index}>
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">User Name</th>
-                    <th scope="col">E Mail</th>
-                    <th scope="col">Phone No.</th>
-                    <th scope="col">Total Consultants</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr key={index}>
-                    {/* <td>{index + 1}</td>
-                    <td>{user}</td>
-                    <td>{(user)}</td>
-                    <td>{user}</td> */}
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          ))}
+      <h1>All Users</h1>
+      <div className="border-shadow">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">User Name</th>
+              <th scope="col">E Mail</th>
+              <th scope="col">Phone No.</th>
+              <th scope="col">Total Consultants</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <img
+                      src={user.image}
+                      alt={`${user.firstName} ${user.lastName}`}
+                      style={{
+                        width: "30px",
+                        height: "30px",
+                        borderRadius: "50%",
+                        marginRight: "10px",
+                      }}
+                    />
+                    {user.firstName} {user.lastName}
+                  </div>
+                </td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>32</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
