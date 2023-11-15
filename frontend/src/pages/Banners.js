8ed704ea1400fc2e52e384869a2803e094445671
atul3007 <atuldwivedi859@gmail.com>
2023-11-15 16:57:39 +0530

    new

--- frontend/src/pages/Banners.js
+++ frontend/src/pages/Banners.js
@@ -2,10 +2,77 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
 const Banners = () => {
+
+  const [banner,setBanner]=useState([]);
  
+  const getBanner=async()=>{
+    try {
+      const {data}=await axios.get("http://localhost:8000/api/all-banner");
+      setBanner(data.bannerbooks);
+      console.log(banner)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    getBanner();
+  },[])
     
   return (
-    <div>Banners</div>
+   <>
+   <h1>Banners</h1>
+   <div className="border-shadow">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Banner Image</th>
+              <th scope="col">Title</th>
+              <th scope="col">Url Link</th>
+              <th scope="col">Banner Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {banner.map((user, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <img
+                      src={user.image}
+                      style={{
+                        width: "30px",
+                        height: "30px",
+                        borderRadius: "50%",
+                        marginRight: "10px",
+                      }}
+                    />
+                  </div>
+                </td>
+                <td>{user.title}</td>
+                <td>{user.linkUrl}</td>
+                <td>{user.bannerInfo}</td>
+                <td>
+                  <span
+                    style={{ cursor: "pointer", marginRight: "10px" }}
+                    // onClick={() => handleEdit(user.id)}
+                  >
+                    ✏️
+                  </span>
+                  <span
+                    style={{ cursor: "pointer" }}
+                    // onClick={() => handleDelete(user.id)}
+                  >
+                    🗑️
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+   </>
   )
 }
 
